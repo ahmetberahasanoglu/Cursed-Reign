c21c737a7468a33238ebf4bbefe4161d078cc40e
ahmetberahasanoglu <127893330+ahmetberahasanoglu@users.noreply.github.com>
2024-09-04 19:27:09 +0300

    Update Wizard.cs

--- Assets/Wizard.cs
+++ Assets/Wizard.cs
@@ -1,19 +1,12 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
-using UnityEditor.Rendering;
 using UnityEngine;
-using static skeleton; // Eðer `skeleton` sýnýfýndaki bir þeye ihtiyacýnýz yoksa, bu satýrý kaldýrabilirsiniz.
 
 [RequireComponent(typeof(Rigidbody2D), typeof(TouchDirection))]
 public class Wizard : MonoBehaviour
 {
     #region Public degiskenler
-    public Transform raycast;
-    public LayerMask raycastMask;
-    public float rayCastLength;
     public HealthBar healthBar;
-    public float attackDistance;
     public DetectionZone attackZone;
     public DetectionZone cliffDetection;
     public float moveSpeed;
@@ -21,54 +14,29 @@ public class Wizard : MonoBehaviour
     #endregion
 
     #region Privates
-    private RaycastHit2D hit;
     private GameObject target;
     private Animator anim;
-    TouchDirection touchDirection;
-    Damageable damageable;
-    Rigidbody2D rb;
-    private float distance;
-    private bool attackMode;
-    private bool inRange;
-    private bool cooling;
-    private float initTimer;
+    private TouchDirection touchDirection;
+    private Damageable damageable;
+    private Rigidbody2D rb;
     #endregion
 
     private void Awake()
     {
-        initTimer = timer;
         anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
         touchDirection = GetComponent<TouchDirection>();
+        damageable = GetComponent<Damageable>();  // damageable'ý burada baþlatýyoruz.
         healthBar.SetHealth(damageable.Health, damageable.MaxHealth);
     }
+
     private void Update()
     {
         HasTarget = attackZone.detectedColliders.Count > 0;
         healthBar.SetHealth(damageable.Health, damageable.MaxHealth);
-        if (inRange)
-        {
-            hit = Physics2D.Raycast(raycast.position, Vector2.left, rayCastLength, raycastMask);
-            RaycastDebugger();
-        }
-
-        if(hit.collider!=null)
-        {
-            EnemyLogic();
-        }
-        else if (hit.collider == null)
-        {
-            inRange = false;
-
-        }
-        if (inRange == false)
-        {
-            anim.SetBool("canMove", false);
-            StopAttack();
-        }
     }
-    private bool hasTarget = false;
 
+    private bool hasTarget = false;
 
     public bool HasTarget
     {
@@ -79,99 +47,54 @@ public class Wizard : MonoBehaviour
         private set
         {
             hasTarget = value;
-            anim.SetBool(AnimStrings.hasTarget, value);
+            anim.SetBool("hasTarget", value);
         }
-
     }
+
+    public bool CanMove
+    {
+        get
+        {
+            return anim.GetBool("canMove");
+        }
+    }
+
     private void FixedUpdate()
     {
-        if (touchDirection.IsOnWall && touchDirection.IsGrounded)        
+        if (touchDirection.IsOnWall && touchDirection.IsGrounded)
         {
             FlipDirection();
         }
-    }
 
-    private void EnemyLogic()
-    {
-        distance = Vector2.Distance(transform.position, target.transform.position);
-        if (distance > attackDistance)
+        if (!damageable.IsHit && touchDirection.IsGrounded)
         {
             Move();
-            StopAttack();
         }
-        else if (attackDistance>=distance &&cooling==false)
-        {
-            AttackFunc();
-        }
-        if (cooling)
-        {
-            Cooldown();
-            anim.SetBool("hasTarget", false);
-        }
-    }
-
-    private void AttackFunc()
-    {
-        timer = initTimer;
-        attackMode = true;
-
-        anim.SetBool("canMove", false);
-        anim.SetBool("hasTarget", true);
     }
 
     private void Move()
     {
         anim.SetBool("canMove", true);
-        if (!anim.GetCurrentAnimatorStateInfo(0).IsName("hasTarget"))
+        if (target != null && !anim.GetCurrentAnimatorStateInfo(0).IsName("hasTarget"))
         {
-            Vector2 targetPosition = new Vector2(target.transform.position.x,transform.position.y);
-            transform.position=Vector2.MoveTowards(transform.position,targetPosition,moveSpeed*Time.deltaTime);
+            Vector2 targetPosition = new Vector2(target.transform.position.x, transform.position.y);
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
         }
-    }
-
-    private void StopAttack()
-    {
-     cooling = false;
-        attackMode= false;
-        anim.SetBool("hasTarget", false);
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "Player")
         {
-            target=collision.gameObject;
+            target = collision.gameObject;
         }
     }
-    void RaycastDebugger()
-    {
-        if (distance > attackDistance)
-        {
-            Debug.DrawRay(raycast.position,Vector2.left*rayCastLength,Color.red);
-        }
-        else if(attackDistance>distance) {
-            Debug.DrawRay(raycast.position, Vector2.left * rayCastLength, Color.green);
-        }
 
-    }
-    public void TriggerCooling()
-    {
-        cooling=true;
-    }
-    void Cooldown()
-    {
-        timer -=Time.deltaTime;
-
-        if(timer<=0&& cooling&&attackMode)
-        {
-            cooling = false;
-            timer = initTimer;
-        }
-    }
     public void OnHit(int damage, Vector2 knockback)
     {
         rb.velocity = new Vector2(knockback.x, rb.velocity.y + knockback.y);
     }
+
     public void OnCliffDetected()
     {
         if (touchDirection.IsGrounded)
@@ -179,6 +102,7 @@ public class Wizard : MonoBehaviour
             FlipDirection();
         }
     }
+
     private void FlipDirection()
     {
         // Karakterin yönünü deðiþtir
